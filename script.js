219fcebd6c43e61e8be38d2e6c68c939842b5852
Ethan Huston <71285039+EthanHuston@users.noreply.github.com>
2025-06-24 14:51:26 -0400

    Making each mouseover event increase opacity
    Functionality is now for the squares to begin at 0
    opacity and each mouseover event increases this by
    0.1
    
    Erasing will reset each square to 0 opacity

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Globals
 let rowColNum = 16;
 let drawMode = "black";
 const gridHeight = 760;
@@ -9,8 +10,9 @@ const changeSizeBtn = document.querySelector(".grid-change-btn");
 const rainbowBtn = document.querySelector("#rainbow-btn");
 const blackBtn = document.querySelector("#black-btn");
 const eraseBtn = document.querySelector("#eraser-btn");
+const gridSizeText = document.querySelector(".grid-size-text");
 
-//Listeners
+//Listener for changing size of grid on button press
 changeSizeBtn.addEventListener("click", () => {
   const newSize = parseInt(
     prompt(
@@ -27,13 +29,15 @@ changeSizeBtn.addEventListener("click", () => {
     rowColNum = newSize;
     makeGrid();
   }
+  gridSizeText.textContent = `${rowColNum} x ${rowColNum}`;
 });
 
+//Draw mode changing listeners
 rainbowBtn.addEventListener("click", () => (drawMode = "rainbow"));
 eraseBtn.addEventListener("click", () => (drawMode = "eraser"));
 blackBtn.addEventListener("click", () => (drawMode = "black"));
 
-//Functions
+//Creates fresh *x* grid based on current rowNumCol value for grid size
 function makeGrid() {
   gridContainer.innerHTML = "";
   for (let i = 0; i < rowColNum ** 2; i++) {
@@ -48,7 +52,14 @@ function makeGrid() {
   }
 }
 
+//Changes the color and opacity of a grid square based on draw mode
+//Passed as a mouseover event callback on all grid squares
 function changeColor(square) {
+  let opacity = parseFloat(
+    window.getComputedStyle(square).getPropertyValue("opacity")
+  );
+  opacity += 0.1;
+  square.style.opacity = opacity;
   switch (drawMode) {
     case "black":
       square.style.backgroundColor = "black";
@@ -58,17 +69,19 @@ function changeColor(square) {
       break;
     case "eraser":
       square.style.backgroundColor = "white";
+      square.style.opacity = 0;
       break;
     default:
       break;
   }
 }
 
+//Returns a random color in css rgb format
 function randomColor() {
   return `rgb(${Math.round(Math.random() * 255)}, ${Math.round(
     Math.random() * 255
   )}, ${Math.round(Math.random() * 255)})`;
 }
 
-//Start on load
+//Makes grid on load
 makeGrid(rowColNum);
